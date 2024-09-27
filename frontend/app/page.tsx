3df0cd7c150f9e8687114deffba744240651f7fd
Facundo Ríos <Facundorios2005@gmail.com>
2024-09-26 23:32:14 -0300

    frontend: init project with NextJS framework

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+function HomePage() {
+  return (
+    <div>
+      HomePage
+      <Button>Button</Button>
+    </div>
+  );
+}
+
+export default HomePage;
